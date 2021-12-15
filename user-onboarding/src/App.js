b22f2d152cc60803d32c1e9e17f5d647cee9e619
kylerostler <93017344+kylerostler@users.noreply.github.com>
2021-12-15 15:10:54 -0700

    added schema and working on filling out app.js

--- user-onboarding/src/App.js
+++ user-onboarding/src/App.js
@@ -1,7 +1,59 @@
 import logo from './logo.svg';
 import './App.css';
+import Form from './Form';
+import schema from './formSchema';
+import * as yup from 'yup';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const initialFormValues = {
+  username: '',
+  email: '',
+  password: '',
+  terms: false,
+}
+
+const initialFormErrors = {
+  username: '',
+  email: '',
+  password: '',
+  terms: '',
+}
+
+const initialUsers = [];
 
 function App() {
+  //states
+  const [users, setUsers] = useState(initialUsers);
+  const [formValues, setFormValues] = useState(initialFormValues);
+  const [formErrors, setFormErrors] = useState(initialFormErrors);
+
+  //helpers
+  const getUsers = () => {
+    axios.get(`https://reqres.in/api/users`)
+    .then(response => {
+      console.log(response)
+    })
+    .catch(error => console.error(error))
+  }
+
+  const postNewUser = newUser => {
+    axios.post(`https://reqres.in/api/users`, newUser)
+    .then(response => {
+      setUsers([ response.data, ...users])
+    }).catch(error => console.error(error))
+    .finally(() => setFormValues(initialFormValues))
+  }
+  
+  const validate = (name, value) => {
+    yup.reach(schema, name)
+    .validate(value)
+    .then(() => setFormErrors({ ...formErrors, [name]: ''}))
+    .catch(err => setFormErrors({ ...formErrors, [name]: err.errors[0]}))
+  }
+  //event handlers
+  //side effects
+  //return
   return (
     <div className="App">
       <header className="App-header">
---
+++ user-onboarding/src/formSchema.js
@@ -0,0 +1,25 @@
+import * as yup from 'yup';
+
+const formSchema = yup.object().shape({
+    username: yup
+    .string()
+    .trim()
+    .required('username is required')
+    .min(5, 'username must be at least 5 characters'),
+
+    email: yup
+    .string()
+    .email('must be a valid email')
+    .required('you must enter an email'),
+
+    password: yup
+    .string()
+    .required('please enter a password')
+    .matches(/^.*(?=.{8,})((?=.*[!@#$%^&*()\-_=+{};:,<.>]){1})(?=.*\d)((?=.*[a-z]){1})((?=.*[A-Z]){1}).*$/,
+    "Password must contain at least 8 characters, one uppercase, one number and one special case character"
+  ),
+
+  terms: yup.boolean(),
+
+})
+export default formSchema;
